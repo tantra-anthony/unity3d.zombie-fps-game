76ca781c3837ff5c788481739d8ca3ad0588a2a3
tantra-anthony <tantra.anthony@gmail.com>
2018-07-24 16:35:21 +0800

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Transform playerSpawnPoints; //parent of spawn points
+    public bool respawn;
+
+    private Transform[] spawnPoints;
+    private bool lastToggle = false;
+
+	// Use this for initialization
+	void Start () {
+        spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform>();
+        print(spawnPoints.Length);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lastToggle != respawn) {
+            Respawn();
+            respawn = false;
+        } else {
+            lastToggle = respawn;
+        }
+	}
+
+    private void Respawn() {
+        int i = Random.Range(1, spawnPoints.Length + 1);
+        transform.position = spawnPoints[i].transform.position;
+    }
+}
